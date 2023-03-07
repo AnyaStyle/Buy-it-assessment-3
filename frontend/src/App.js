9ca115286a3edde68c2eb5489383bf54ff62cfa9
anya <anya.radkevich@gmail.com>
2023-03-07 14:51:10 +1300

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">buy it</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
