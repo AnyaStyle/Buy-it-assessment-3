f41e1af1be88eb838a513f54144f44e09b74d841
anya <anya.radkevich@gmail.com>
2023-03-10 22:20:13 +1300

    Loading Box, Messege Box, Product not find

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get('/api/products/slug/:slug', (req, res) => {
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ messege: 'Product Not Found' });
+    res.status(404).send({ message: 'Product Not Found' });
   }
 });
 
