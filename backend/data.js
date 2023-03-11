d13fd99ad7bd0c93c5b0b325e8ff4edeb3945982
anya <anya.radkevich@gmail.com>
2023-03-11 21:50:02 +1300

    seed sample users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Anya',
+      email: 'admin@buyit.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Mihee',
+      email: 'user@buyit.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
